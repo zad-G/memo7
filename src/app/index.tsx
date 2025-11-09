10425cd4e5d68a3b99a474dd95566813e4828c0f
Ame <236023242+zad-G@users.noreply.github.com>
2025-11-09 15:11:05 +0900

    Install expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+const Index = (): React.JSX.Element => {
+    return(
+        <View>
+            <Text>Cat is Cute</Text>
+        </View>
+    )
+}
+
+export default Index
