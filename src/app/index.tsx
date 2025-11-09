77255f6a4c56822e0a22544eb1dac717be14a1bd
Ame <236023242+zad-G@users.noreply.github.com>
2025-11-09 19:52:55 +0900

    Index TSX Content

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 const Index = (): React.JSX.Element => {
     return(
-        <View>
-            <Text>Cat is Cute</Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View>
+
+
+            <View>
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>202510月1日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>202510月1日 10:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>202510月1日 10:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                        </View>
+                    </View>
         </View>
+
+        <View>
+        <Text>+</Text>
+        </View>
+    </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+})
 
 export default Index
